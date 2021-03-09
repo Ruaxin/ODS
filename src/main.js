879278e2c848b82c4235b786f2de2d46fa290879
Ruaxin <qy.xin@foxmail.com>
2021-03-09 09:49:17 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 将全局的echarts对象挂载到Vue的原型对象上
+Vue.prototype.$echarts = window.echarts
 
 Vue.config.productionTip = false
 

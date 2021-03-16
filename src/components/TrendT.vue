e824b370193d514e631b4468c6b2d799817f0cb8
Ruaxin <qy.xin@foxmail.com>
2021-03-16 16:43:29 +0800

    trend的本地版

--- src/components/TrendT.vue
+++ src/components/TrendT.vue
@@ -1,9 +1,121 @@
 <template>
-  <div></div>
+  <div class="com-container">
+    <div class="com-chart" ref="trend_ref"></div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.screenAdapter)
+  },
+  methods: {
+    initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.trend_ref)
+      const initOption = {
+        grid: {
+          left: '3%',
+          top: '35%',
+          right: '4%',
+          bottom: '1%',
+          containLabel: true
+        },
+        tooltip: {
+          type: 'axis'
+        },
+        legend: {
+          left: 20,
+          top: '15%',
+          icon: 'circle'
+        },
+        xAxis: {
+          type: 'category'
+        },
+        yAxis: {
+          type: 'value'
+        }
+      }
+      this.chartInstance.setOption(initOption)
+    },
+    async getData() {
+      const {data: res} = await this.$http.get('trend.json')
+      this.allData = res
+      this.updateChart()
+    },
+    updateChart() {
+      const colorArr1 = [
+        'rgba(11,168,44,0.5)',
+        'rgba(44,110,255,0.5)',
+        'rgba(22,242,217,0.5)',
+        'rgba(254,33,30,0.5)',
+        'rgba(250,105,0,0.5)'
+      ]
+      const colorArr2 = [
+        'rgba(11,168,44,0)',
+        'rgba(44,110,255,0)',
+        'rgba(22,242,217,0)',
+        'rgba(254,33,30,0)',
+        'rgba(250,105,0,0)'
+      ]
+      const timeArr = this.allData.common.month
+      const valueArr = this.allData.map.data
+      const seriesArr = valueArr.map((item, index) => {
+        return {
+          name: item.name,
+          type: 'line',
+          data: item.data,
+          stack: 'map',
+          areaStyle: {
+            color: {
+              type: 'linear',
+              x: 0,
+              y: 0,
+              x2: 0,
+              y2: 1,
+              colorStops: [{
+                offset: 0, color: colorArr1[index]
+              }, {
+                offset: 1, color: colorArr2[index]
+              }],
+            }
+          }
+        }
+      })
+      const legendArr = valueArr.map(item => {
+        return item.name
+      })
+      const dataOption = {
+        xAxis: {
+          data: timeArr
+        },
+        legend: {
+          data: legendArr
+        },
+        series: {
+          seriesArr
+        }
+      }
+      this.chartInstance.setOption(dataOption)
+    },
+    screenAdapter() {
+      const adapterOption = {}
+      this.chartInstance.setOption(adapterOption)
+      this.chartInstance.resize()
+    }
+  }
+}
 </script>
 
 <style scoped>

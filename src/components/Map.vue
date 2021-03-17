ce63ce21c58bb62b6c74e4e0fd6bf84f8e54a26f
Ruaxin <qy.xin@foxmail.com>
2021-03-17 15:54:48 +0800

    初始化map组件

---
+++ src/components/Map.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="map_ref"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.screenAdapter)
+  },
+  methods: {
+    initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.map_ref)
+      const initOption = {}
+      this.chartInstance.setOption(initOption)
+    },
+    async getData() {
+      this.updateChart()
+    },
+    updateChart() {
+      const dataOption = {}
+      this.chartInstance.setOption(dataOption)
+    },
+    screenAdapter() {
+      const adapterOption = {}
+      this.chartInstance.setOption(adapterOption)
+      this.chartInstance.resize()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

ae47a5ce0b0e2598722117f42ce0017701066446
Ruaxin <qy.xin@foxmail.com>
2021-03-20 20:49:50 +0800

    初始化Rank

---
+++ src/components/RankT.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="rank_ref"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null,
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.screenAdapter)
+  },
+  methods: {
+    // 初始化echartsInstance对象
+    initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.rank_ref, 'chalk')
+      // 对图表初始化配置的控制
+      const initOption = {}
+      this.chartInstance.setOption(initOption)
+    },
+    // 获取服务器的数据
+    async getData() {
+      const {data: ret} = await this.$http.get('rank')
+      this.allData = ret
+      this.updateChart()
+    },
+    // 更新图表
+    updateChart() {
+      const dataOption = {}
+      this.chartInstance.setOption(dataOption)
+    },
+    // 自适应屏幕大小
+    screenAdapter() {
+      // const titleFontSize = this.$refs.rank_ref.offsetWidth / 100 * 3.6
+      const adapterOption = {}
+      this.chartInstance.setOption(adapterOption)
+      // 调用resize方法
+      this.chartInstance.resize()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/RankPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="com-page">
+    <rank></rank>
+  </div>
+</template>
+
+<script>
+import Rank from '@/components/Rank.vue'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    rank: Rank
+  },
+  methods: {},
+
+}
+</script>
+
+<style scoped>
+
+</style>

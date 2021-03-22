7c12c9d8897ca167626c9f073807d25b5998043e
Ruaxin <qy.xin@foxmail.com>
2021-03-22 20:35:24 +0800

    实现饼图的标题显示和切换

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="com-container">
     <div class="com-chart" ref="hot_ref"></div>
+    <span class="iconfont arr-left" @click="toLeft">&#xe6ef;</span>
+    <span class="iconfont arr-right" @click="toRight">&#xe6ed;</span>
+    <span class="cat-name">{{ catName }}</span>
   </div>
 </template>
 
@@ -10,6 +13,7 @@ export default {
     return {
       chartInstance: null,
       allData: null,
+      currentIndex: 0 // 当前所展示出的一级分类数据
     }
   },
   mounted() {
@@ -20,6 +24,15 @@ export default {
   },
   destroyed() {
     window.removeEventListener('resize', this.screenAdapter)
+  },
+  computed: {
+    catName() {
+      if (!this.allData) {
+        return ''
+      } else {
+        return this.allData[this.currentIndex].name
+      }
+    }
   },
   methods: {
     // 初始化echartsInstance对象
@@ -41,10 +54,10 @@ export default {
     },
     // 更新图表
     updateChart() {
-      const legendDate = this.allData[0].children.map(item => {
+      const legendDate = this.allData[this.currentIndex].children.map(item => {
         return item.name
       })
-      const seriesData = this.allData[0].children.map(item => {
+      const seriesData = this.allData[this.currentIndex].children.map(item => {
         return {
           name: item.name,
           value: item.value
@@ -69,11 +82,45 @@ export default {
       this.chartInstance.setOption(adapterOption)
       // 调用resize方法
       this.chartInstance.resize()
+    },
+    toLeft() {
+      this.currentIndex--
+      if (this.currentIndex < 0) {
+        this.currentIndex = this.allData.length - 1
+      }
+      this.updateChart()
+    },
+    toRight() {
+      this.currentIndex++
+      if (this.currentIndex > this.allData.length - 1) {
+        this.currentIndex = 0
+      }
+      this.updateChart()
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.arr-left {
+  position: absolute;
+  left: 10%;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
 
+.arr-right {
+  position: absolute;
+  right: 10%;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.cat-name {
+  position: absolute;
+  left: 80%;
+  bottom: 20px;
+}
 </style>

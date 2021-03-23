2a5f0daae6e2cde0394fb6c463c405cfb2b09808
Ruaxin <qy.xin@foxmail.com>
2021-03-23 10:21:21 +0800

    初始化stock组件

---
+++ src/views/StockPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="com-page">
+    <stock></stock>
+  </div>
+</template>
+
+<script>
+import Stock from '@/components/Stock.vue'
+
+export default {
+  components: {stock: Stock}
+}
+</script>
+
+<style scoped>
+
+</style>
